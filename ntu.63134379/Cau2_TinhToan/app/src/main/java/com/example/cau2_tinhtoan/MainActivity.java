01a4bd504d0db183882ec9f00dc4889bdd5643a6
LTK2102 <tuankiet5505@gmail.com>
2024-03-24 22:42:28 +0700

    upload

--- ntu.63134379/Cau2_TinhToan/app/src/main/java/com/example/cau2_tinhtoan/MainActivity.java
+++ ntu.63134379/Cau2_TinhToan/app/src/main/java/com/example/cau2_tinhtoan/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.cau2_tinhtoan;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -12,7 +13,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     Button bt1,bt2,bt3,bt4,bt5,bt6,bt7,bt8,bt9,bt0;
-    Button btbang,btcong,bttru,btnha,btchia,btdot,btc,btce;
+    Button btbang,btcong,bttru,btnhan,btchia,btdot,btc,btce;
     TextView mainsrceen,secondscreen;
     String TinhToan;
     String pheptoan;
@@ -22,17 +23,79 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        anhxa();
+        btc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                secondscreen.setText("");
+                mainsrceen.setText("");
+                TinhToan = "";
+                pheptoan = "";
+                checkDot = false;
+                checkBang = false;
+            }
         });
+        bt0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!checkBang){
+                    TinhToan = mainsrceen.getText().toString();
+                    mainsrceen.setText(TinhToan + "0");
+                }
+            }
+        });
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!checkBang){
+                    TinhToan = mainsrceen.getText().toString();
+                    mainsrceen.setText(TinhToan + "1");
+                }
+            }
+        });
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!checkBang){
+                    TinhToan = mainsrceen.getText().toString();
+                    mainsrceen.setText(TinhToan + "2");
+                }
+            }
+        });
+        bt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!checkBang){
+                    TinhToan = mainsrceen.getText().toString();
+                    mainsrceen.setText(TinhToan + "3");
+                }
+            }
+        });
+
     }
-    protected void anhxa(){
+
+    protected void anhxa()
+    {
         bt0 = findViewById(R.id.so0);
-
-
+        bt1 = findViewById(R.id.bt1);
+        bt2 = findViewById(R.id.bt2);
+        bt3 = findViewById(R.id.bt3);
+        bt4 = findViewById(R.id.bt4);
+        bt5 = findViewById(R.id.bt5);
+        bt6 = findViewById(R.id.bt6);
+        bt7 = findViewById(R.id.bt7);
+        bt8 = findViewById(R.id.bt8);
+        bt9 = findViewById(R.id.bt9);
+        btdot = findViewById(R.id.btdot);
+        btbang = findViewById(R.id.btbang);
+        btcong = findViewById(R.id.btcong);
+        bttru = findViewById(R.id.bttru);
+        btchia = findViewById(R.id.btchia);
+        btnhan = findViewById(R.id.btnhan);
+        btce = findViewById(R.id.btce);
+        btc = findViewById(R.id.btc);
+        mainsrceen = findViewById(R.id.man_hinh);
+        secondscreen = findViewById(R.id.man_hinh_phu);
     }
 }
