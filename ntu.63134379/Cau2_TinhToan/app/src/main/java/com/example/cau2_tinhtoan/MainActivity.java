62c1ff7c9dee6867a198354373a456c4f9fab296
LTK2102 <tuankiet5505@gmail.com>
2024-03-24 21:43:28 +0700

    upload

--- ntu.63134379/Cau2_TinhToan/app/src/main/java/com/example/cau2_tinhtoan/MainActivity.java
+++ ntu.63134379/Cau2_TinhToan/app/src/main/java/com/example/cau2_tinhtoan/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.cau2_tinhtoan;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button bt1,bt2,bt3,bt4,bt5,bt6,bt7,bt8,bt9,bt0;
+    Button btbang,btcong,bttru,btnha,btchia,btdot,btc,btce;
+    TextView mainsrceen,secondscreen;
+    String TinhToan;
+    String pheptoan;
+    boolean checkDot=false, checkBang=false;
+    double so1, so2, ketqua;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    protected void anhxa(){
+        bt0 = findViewById(R.id.so0);
+
+
     }
 }
